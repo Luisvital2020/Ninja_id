ca239927b2258bcfbf300e747a9bdef99623a86a
Luis Vital <luisjuniorista100@gmail.com>
2023-04-21 16:59:02 -0500

    cuarto cambio

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: NinjaCard(),
-));
+void main() { => runApp(MaterialApp
+    home :NinjaCard (),
+  )); //MateriaApp
+  class NinjaCard extend StatelessWidget {
+  @override
+  Widget build (BuilContext context){
+  return Scaffold ();
+      BackgroudColor: Color.grey[900],
+      appBar:AppBar ()
+      tittle : Text('Ninja Id card'),
+      centerTitle : true,
+      backgroundColor: colors.grey [850],
+  elevation: 0.0,
+  ),//AppBar
 
-// ignore: use_key_in_widget_constructors
-class NinjaCard extends StatelessWidget {
-    @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        // ignore: prefer_const_constructors
-        title: Text('Ninja ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        // ignore: prefer_const_constructors
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
+  body: padding (
+    padding:EdgeInsets.fromLTRB (30.0, 40.0, 30.0, 0.0),
+  child: column(
+    children <Widget> [
 
-        ),
-      ),
-    );
+  text(
+   'NAME'
+    Style: TextStyle(
+    color:Colors.grey,
+    letterSpacing: 2.0,
+  )// TextStyle
+  )//text
+    sizebox(height:10.0),
+  text(
+   'chun-li',
+    Style :TextStyle(
+    color:Colors.amberAccent[200],
+    letterSpacing: 2.0,
+    fontSize: 28.8,
+    fontWeight:FontWeight.bold
+  )//TextStyle
+  )//text
+
   }
-}
-
+  }
